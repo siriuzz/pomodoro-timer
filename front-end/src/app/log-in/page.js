de04e8f7b62c6b8afa43f2a86a670291ace804a9
siriuzz <elianmatosd03@gmail.com>
2024-03-20 01:02:41 -0400

    log-in y sign-in diseñados

---
+++ front-end/src/app/log-in/page.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { MainInput } from '@/components/MainInput'
+import { FormButton } from '@/components/FormButton'
+import Link from 'next/link'
+
+export default function LogInPage() {
+  return (
+    <div className="grid place-items-center h-screen">
+        <div className='border-blue-400 border-2 rounded-lg p-4'>
+            <p className='text-center text-xl font-bold my-5 text-blue-500'>Iniciar sesión</p>
+            <MainInput placeholder={'Correo electrónico'}></MainInput>
+            <MainInput placeholder={'Contraseña'}></MainInput>
+            <Link href={'/sign-in'} className='text-blue-800 pb-10'>Crear cuenta</Link>
+            <FormButton text="Ingresar"></FormButton>
+        </div>
+    </div>
+  )
+}
---
+++ front-end/src/components/FormButton.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const FormButton = ({text,  ...rest}) => {
+  return (
+    <div className='mb-3'>
+        <button className='bg-blue-200 text-blue-600 font-bold w-full rounded-lg p-3'>
+            {text}
+        </button>
+
+    </div>
+  )
+}
