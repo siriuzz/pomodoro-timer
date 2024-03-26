35f5e30257e8e9cf80229b623590bcc76e66fdb6
siriuzz <elianmatosd03@gmail.com>
2024-03-26 00:01:07 -0400

    express eliminado

--- front-end/src/app/(auth)/log-in/page.js
+++ front-end/src/app/(auth)/log-in/page.js
@@ -2,8 +2,20 @@ import React from 'react'
 import { MainInput } from '@/components/forms'
 import { FormButton } from '@/components/forms'
 import Link from 'next/link'
+import axios from 'axios'
 
 export default function LogInPage() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const login = () =>{
+    try {
+      axios.post()
+    } catch (error) {
+      
+    }
+  }
+
   return (
     <div className="grid place-items-center h-screen">
         <div className='border-blue-400 border-2 rounded-lg p-4'>
