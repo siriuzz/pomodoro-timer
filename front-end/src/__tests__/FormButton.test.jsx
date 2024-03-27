89cc172e2cccf77a1025d4aeb036d067a644edc6
siriuzz <elianmatosd03@gmail.com>
2024-03-27 03:03:43 -0400

    jest implementado y test basico

---
+++ front-end/src/__tests__/FormButton.test.jsx
@@ -0,0 +1,13 @@
+import '@testing-library/jest-dom'
+import { render, screen } from '@testing-library/react'
+import FormButton from '../components/forms/FormButton'
+ 
+describe('FormButton', () => {
+  it('renders a FormButton component', () => {
+    render(<FormButton text={"test"} />)
+ 
+    const button = screen.getByText('test');
+ 
+    expect(button).toBeInTheDocument()
+  })
+})
