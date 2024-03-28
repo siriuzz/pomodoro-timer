e870c0e13ce7445a36936bd0cc38b0f898b52775
siriuzz <elianmatosd03@gmail.com>
2024-03-27 23:41:38 -0400

    signin funcionando

--- front-end/src/app/(auth)/sign-in/page.js
+++ front-end/src/app/(auth)/sign-in/page.js
@@ -1,20 +1,12 @@
 import React from 'react'
-import { MainInput, FormButton } from '@/components/forms';
+import { SigninForm } from '@/components/forms';
 import Link from 'next/link'
 
 
 export default function SignInPage() {
   return (
-    <div className="grid place-items-center h-screen">
-        <div className='border-blue-400 text-blue-600 border-2 rounded-lg p-4 h-auto'>
-            <p className='text-center text-xl font-bold my-5 text-blu'>Sign In</p>
-            <MainInput placeholder={'Nombre'}></MainInput>
-            <MainInput placeholder={'Correo electrónico'}></MainInput>
-            <MainInput placeholder={'Contraseña'}></MainInput>
-            <MainInput placeholder={'Repetir contraseña'}></MainInput>
-            <p className='text-black'>¿Ya tienes una cuenta? </p><div className='mb-4'><Link href={'/log-in'} className='font-bold' >Ingresa aquí</Link></div>
-            <FormButton text="Crear cuenta"></FormButton>
-        </div>
+    <div className='w-full'>
+        <SigninForm/>
     </div>
   )
 }
--- front-end/src/components/forms/index.js
+++ front-end/src/components/forms/index.js
@@ -1,3 +1,4 @@
 export { default as FormButton } from "./FormButton";
 export { default as MainInput } from "./MainInput";
 export { default as LoginForm} from "./LoginForm"
+export { default as SigninForm} from "./SigninForm"
