5ea24a3a949111f16e1ef0f1f22ddc1044633818
siriuzz <elianmatosd03@gmail.com>
2024-03-28 01:50:57 -0400

    validaciones y cambios a pagina principal

--- front-end/src/components/forms/MainInput.js
+++ front-end/src/components/forms/MainInput.js
@@ -1,17 +1,16 @@
 import React from "react";
-import { IconButton } from "@/components/ui";
-const MainInput = ({ placeholder, label, error, icon,iconAction,  ...rest }) => {
+
+const MainInput = React.forwardRef(({ placeholder, label, icon, iconAction, ...rest }, ref) => {
   return (
-    <div className="mb-3 flex align-center">
-      {label && (
+    <div className="mb-1 flex align-center">
+      {/* {label && (
         <label className="block text-gray-700 text-sm font-bold mb-2">
           {label}
         </label>
-      )}
+      )} */}
       <input
-        className={`shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${
-          error ? "border-red-500" : "border-gray-300"
-        }`}
+        ref={ref}
+        className={`shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline`}
         placeholder={placeholder}
         {...rest}
       />
@@ -22,11 +21,9 @@ const MainInput = ({ placeholder, label, error, icon,iconAction,  ...rest }) => 
          </button>
         </div>
       ) : null}
-
-      <div className="absolute inset-y-0 right-0 flex items-center pr-3 pointer-events-none"></div>
-      {error && <p className="text-red-500 text-xs italic">{error}</p>}
+      
     </div>
   );
-};
+});
 
 export default MainInput;
